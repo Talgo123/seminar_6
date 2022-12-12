43ecffe56cf598e13c222c7bc68820fe8b4ee7b5
talgat <15talgat@mail.ru>
2022-12-12 14:07:03 +0300

    done zadacha_3

---
+++ zadacha_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+int number = new Random().Next(1, 100);
+System.Console.WriteLine(number);
+GetBinaryView(number);
+
+void GetBinaryView(int number)
+{
+    if (number <= 0)
+    {
+        return;
+    }
+    GetBinaryView(number 2 /);
+    Console.WriteLine(number % 2);
+}
