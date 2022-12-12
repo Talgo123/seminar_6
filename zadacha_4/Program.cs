1b334f2a275dd32bd4a12f997bd0c0d9c9d0700d
talgat <15talgat@mail.ru>
2022-12-12 14:50:25 +0300

    done zadacha 4

---
+++ zadacha_4/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("введите число: ");
+int num = int.Parse(Console.ReadLine());
+int[] array = Fibonacci(num);
+System.Console.WriteLine('[' + string.Join(",", array) + ']');
+
+
+
+int[] Fibonacci(int n)
+{
+    int[] arr = new int[n];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <arr.Length; i++)
+    {
+        arr[i] = arr[i - 2] + arr[i - 1];
+    }
+    return arr;
+}
