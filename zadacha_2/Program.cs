1c10991d28695b02a5fd397f67f99697d32c2789
talgat <15talgat@mail.ru>
2022-12-12 14:06:40 +0300

    done_zadacha_2

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+int EntryNumber(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void Triangle(int a, int b, int c)
+{
+   if (a + b < c || a + c < b || b + c < a)
+   {
+    System.Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c} существует");
+   }
+   else
+   {
+    System.Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c}  не существует");
+   }
+}
+
+int firstSide = EntryNumber("Введите первую сторону треугольника: ");
+int secondSide = EntryNumber("Введите вторую сторону треугольника: ");
+int thirdSide = EntryNumber("Введите третью сторону треугольника: ");
+
+Triangle(firstSide, secondSide, thirdSide);
+
